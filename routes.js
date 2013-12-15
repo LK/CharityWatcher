7236d11fefe6a41c0abc6a504902cfdb8f4182c0
JFKingsley <silverarrow23@kingsleys.org>
2013-12-15 00:17:54 +0000

    Using json.stringify now..

--- routes.js
+++ routes.js
@@ -7,12 +7,12 @@ exports.getCampaignCode = function(req, res, next){
 	request('http://my.charitywater.org/'+req.body.campaignName, function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
 	    var pattern = /campaign_id\s*=\s*(\d+)/;
-	    res.send(200,'{"status": "completed", "response": "'+pattern.exec(body)[0].substring(12)+'"}');
+	    res.send(200,JSON.stringify(status: "completed", response: pattern.exec(body)[0].substring(12)));
 	  }else{
-      res.send(400,'{"status": "failed", "reason": "invalid campaignName"}');
+      res.send(400,JSON.stringify(status: "failed", reason: "invalid campaign name"));
 	  }
 	});
   }else{
-  res.send(400,'{"status": "failed", "reason": "empty campaignName"}');
+  res.send(400,JSON.stringify(status: "failed", reason: "empty campaign name"));
   }
 }
