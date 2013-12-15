f8d6f25fe8b77f0a1a16317d11b49736e962e865
Nathaniel Higgins <nat@nath.is>
2013-12-15 02:54:33 +0000

    Lets add some code seperation

--- routes.js
+++ routes.js
@@ -1,28 +1,16 @@
-var async = require('async');
-var cheerio = require('cheerio');
-var request = require('request');
-
 exports.getCampaignCode = function(req, res, next) {
-	if (typeof req.body.campaignName !== 'undefined') {
-		request('http://my.charitywater.org/' + req.body.campaignName, function(error, response, body) {
-			if (!error && response.statusCode == 200) {
-				var pattern = /campaign_id\s*=\s*(\d+)/;
-				
-				res.send(200, JSON.stringify({
-					status: 'completed',
-					response: body.match(pattern)[1]
-				}));
-			} else {
-				res.send(400, JSON.stringify({
-					status: 'failed',
-					reason: 'invalid campaign name'
-				}));
-			}
+
+	if (typeof req.body.campaignName === 'undefined') {
+		res.send(400, {
+			status: 'failed',
+			reason: 'Empty campaign name'
 		});
 	} else {
-		res.send(400, JSON.stringify({
-			status: 'failed',
-			reason: 'empty campaign name'
-		}));
+		require('./lib/campaign')(req.body.campaignName).id()
+			.then(function(id) {
+				res.send(200, { status: 'completed', response: id });
+			}, function(reason) {
+				res.send(400, { status: 'failed', reason: reason.toString() })
+			}).done();
 	}
 }
