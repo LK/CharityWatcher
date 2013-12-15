86c7ed724ea4d66f8b8ad9eef60910db6886dcd9
Nathaniel Higgins <nat@nath.is>
2013-12-15 00:40:08 +0000

    Clean up code

--- routes.js
+++ routes.js
@@ -2,17 +2,27 @@ var async = require('async');
 var cheerio = require('cheerio');
 var request = require('request');
 
-exports.getCampaignCode = function(req, res, next){
-	if(req.body.campaignName != undefined){
-	request('http://my.charitywater.org/'+req.body.campaignName, function (error, response, body) {
-	  if (!error && response.statusCode == 200) {
-	    var pattern = /campaign_id\s*=\s*(\d+)/;
-	    res.send(200,JSON.stringify(status: "completed", response: pattern.exec(body)[0].substring(12)));
-	  }else{
-      res.send(400,JSON.stringify(status: "failed", reason: "invalid campaign name"));
-	  }
-	});
-  }else{
-  res.send(400,JSON.stringify(status: "failed", reason: "empty campaign name"));
-  }
+exports.getCampaignCode = function(req, res, next) {
+	if (typeof req.body.campaignName !== 'undefined') {
+		request('http://my.charitywater.org/' + req.body.campaignName, function(error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var pattern = /campaign_id\s*=\s*(\d+)/;
+				
+				res.send(200, JSON.stringify({
+					status: 'completed',
+					response: body.match(pattern)[1]
+				}));
+			} else {
+				res.send(400, JSON.stringify({
+					status: 'failed',
+					reason: 'invalid campaign name'
+				}));
+			}
+		});
+	} else {
+		res.send(400, JSON.stringify({
+			status: 'failed',
+			reason: 'empty campaign name'
+		}));
+	}
 }
