d9390a761cb08fff7938c88ca3a0e93732ab9b57
Nathaniel Higgins <nat@nath.is>
2013-12-19 00:15:44 +0000

    Make it more restful, also clean up the controller a bit

--- app.js
+++ app.js
@@ -6,11 +6,7 @@ var campaignController = require('./controllers/campaign').get(require('./models
 // Express and Handlebars middlewear Stack
 app.use(express.bodyParser());
 
-app.get('/', function(req, res, next) {
-	// code here :P
-});
-
-app.post('/getCampaignID', campaignController.get);
+app.get('/campaign/:name', campaignController.get);
 
 app.listen(8080, function() {
 	console.log('Listening on port 8080');
--- controllers/campaign.js
+++ controllers/campaign.js
@@ -1,4 +1,6 @@
-var _ = require('underscore');
+var _    = require('underscore');
+var util = require('../lib/util');
+var Q    = require('Q');
 
 function CampaignController(Model) {
     // @todo Can probably clean this up?
@@ -16,18 +18,39 @@ _.extend(CampaignController.prototype, {
 
     get: function(req, res, next) {
 
-        var sendError = this.error.apply(this, arguments);
-        var model     = this.Model.get(req.body.campaignName);
+        var sendError   = this.error.apply(this, arguments);
+        var sendSuccess = this.success.apply(this, arguments);
+        var model       = this.Model.get(req.params.name);
 
-        model.id().then(function(id) {
-            res.json({ status: 'completed', response: id });
-        }).fail(sendError).done();
+        Q.all([model.id()])
+            .spread(this.getCamaignObject)
+            .then(util.wrapObject('campaign'))
+            .then(sendSuccess)
+            .fail(sendError)
+            .done();
+    },
+
+    getCamaignObject: function(id) {
+        return { id: id };
+    },
+
+    success: function(req, res) {
+        return function(response) {
+            res.json({
+                status: 'completed',
+                response: response
+            });
+        }
     },
 
     error: function(req, res) {
         return function(reason) {
 
             if (reason instanceof Error) {
+                // @todo Might want to differentiate between response
+                // errors and code errors somehow.
+                console.error(reason.stack);
+
                 reason = reason.message;
             }
 
